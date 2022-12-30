08b0fad204f0eca6f086535e3accdad009ae8512
akashp1997 <akash.p1997@gmail.com>
2022-12-31 01:38:23 +0530

    remove all HTML views

--- bbu/urls.py
+++ bbu/urls.py
@@ -24,10 +24,5 @@ router.register(r'bbu', views.BbuViewSet)
 
 
 urlpatterns = [
-    path('', views.index, name='homepage'),
-    path('create-project', views.create_project, name='create-project'),
-    path('project/MH-<int:job_no>', views.view_efu, name='project-page'),
-    path('project/MH-<int:job_no>/delete-items', views.delete_bbu_row, name='delete-bbu-rows'),
-    path('project/MH-<int:job_no>/edit-item', views.edit_item, name='edit-item'),
-    path('api/', include(router.urls)),
+    path('api/', include(router.urls))
 ]
